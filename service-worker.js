a11f1db6d9f9b69849736ab6e4015ee5bc3c5f40
Danny Moerkerke <danny@dannymoerkerke.nl>
2020-10-05 19:03:41 +0200

    - updated text content - added containment css for images.html - removed redundant code

--- service-worker.js
+++ service-worker.js
@@ -33,7 +33,7 @@ const filesToCache = [
   ...staticFiles,
 ];
 
-const version = 53;
+const version = 54;
 const cacheName = `html_cache`;
 const debug = true;
 
@@ -72,6 +72,13 @@ const routes = [
       return `<main>
                 <section id="content">
                   <h2>Blog</h2>
+                  <p>
+                    <em>
+                      This page contains twelve of my blog posting which are fetched dynamically and then combined into
+                      one large HTML page. After the first render it is cached and served from IndexedDB for subsequent 
+                      renders.
+                    </em>
+                  </p>
                   ${data.map(({title, intro, body}) => `<article>${title} ${intro} ${body}</article>`).join('')}
                 </section>
               </main>`;
